f2e3e5c47f8e74ee1e8096a01a313d399d3a2c55
VadymGor <07tennis@gmail.com>
2021-01-25 14:26:42 +0100

    creating log_out and json file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from  datetime import datetime
 
 Builder.load_file('design.kv')
 
@@ -9,13 +11,42 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text = "Wrong username or password"
+
 
 class RootWidget(ScreenManager):
     pass
 
 
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        users[uname] = {'username': uname, 'password': pword,
+                        'created': datetime.now().strftime("%Y-%M-%D %H-%M-%S")}
+        with open("users.json", "w") as file:
+            json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login(self):
+        self.manager.direction = "right"
+        self.manager.current = "login_screen"
+
+
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.direction = "right"
+        self.manager.current = "login_screen"
+
 
 class MainApp(App):
     def build(self):
